1674aad0db68004f4cb523b73573458771f70503
Francis Bello <bellofrancis87@gmail.com>
2022-01-26 15:04:01 +0100

    cleaned up the code & added comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Conter Begins
+
 const counters = document.querySelectorAll(".counter");
 const speed = 3000;
 
@@ -17,7 +19,8 @@ counters.forEach((counter) => {
   };
   updateCount();
 });
-
+// Counter Ends
+// Intersection Observer Begins
 const options = { threshold: 0.4 };
 const addActiveClass = (entries, observer) => {
   entries.forEach((entry) => {
@@ -41,7 +44,6 @@ const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
   observer.observe(section);
 });
-
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
 const appearOptions = { threshold: 0, rootMargin: "0px 0px -250px 0px" };
@@ -65,7 +67,8 @@ faders.forEach((fader) => appearOnScroll.observe(fader));
 sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
-
+// Intersection Observer ends
+// Fix nav Starts
 const navBar = document.querySelector(".desktop-nav");
 const navHeight = navBar.getBoundingClientRect().height;
 window.addEventListener("scroll", () => {
@@ -76,3 +79,63 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
+// fix nav ends
+
+// Form Validation Begins
+const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const email = document.getElementById("email");
+const comment = document.getElementById("comment");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  const fnameValue = fname.value.trim();
+  const emailValue = email.value.trim();
+  const commentValue = comment.value.trim();
+
+  if (fnameValue === "") {
+    setError(fname, "Fullname is required");
+  } else {
+    setSuccess(fname);
+  }
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    seError(email, "Provide a valid email address");
+  } else {
+    setSuccess(email);
+  }
+
+  if (commentValue === "") {
+    setError(comment, "Message is required");
+  } else {
+    setSuccess(comment);
+  }
+};
